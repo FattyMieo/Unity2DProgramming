9fdc86e0d3b8c5c5569fce1846d88907fc8d6ef3
FattyMieo <fong2009@live.com.my>
2017-07-27 11:06:54 +0800

    Bounding Box

---
+++ Assets/CustomCollisionScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCollisionScript : MonoBehaviour
+{
+    SpriteRenderer spr;
+    public CustomCollisionScript other;
+
+    public bool isPixelCollision = false;
+    private bool isColliding = false;
+    public bool debugMode = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.spr = this.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!isPixelCollision)
+        {
+            if(this.spr.bounds.Intersects(other.spr.bounds))
+            {
+                isColliding = true;
+            }
+            else
+            {
+                isColliding = false;
+            }
+        }
+
+        DebugRenderer();
+    }
+
+    //Render sprite red if collison is detected
+    void DebugRenderer()
+    {
+        if(debugMode)
+        {
+            if(isColliding)
+            {
+                spr.color = Color.red;
+            }
+            else
+            {
+                spr.color = Color.white;
+            }
+        }
+    }
+}
