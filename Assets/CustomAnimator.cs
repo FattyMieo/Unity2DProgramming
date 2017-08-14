720ced609ab3c1003001dfb43d63cdb960841309
FattyMieo <fong2009@live.com.my>
2017-08-14 21:17:42 +0800

    Finished Assignment

--- Assets/CustomAnimator.cs
+++ Assets/CustomAnimator.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class CustomAnimator : MonoBehaviour
 {
+	public enum AnimatorPlayMode
+	{
+		ForwardLoop,
+		ReverseLoop,
+		PlayOneShot,
+		PingPong,
+		MultiRow,
+
+		Total
+	}
+
+	public enum LoopMode
+	{
+		OneShot,
+		Loop,
+		PingPong,
+
+		Total
+	}
+
 	[Header("Sprite Setup")]
 	public int colSize;
 	public int rowSize;
@@ -16,6 +36,13 @@ public class CustomAnimator : MonoBehaviour
 	[Header("Time")]
 	public float timer;
 	public int framesPerSecond;
+
+	[Header("Play Mode")]
+	[ContextMenuItem( "Update settings to this preset", "ChangeMode" )]
+	public AnimatorPlayMode mode;
+	public bool reversed;
+	public LoopMode loopMode;
+	public bool useMultiRow;
 
 	// Use this for initialization
 	void Start ()
@@ -38,6 +65,64 @@ public class CustomAnimator : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
+		if(Input.GetKey(KeyCode.Alpha1)) SwitchMode(AnimatorPlayMode.ForwardLoop);
+		if(Input.GetKey(KeyCode.Alpha2)) SwitchMode(AnimatorPlayMode.ReverseLoop);
+		if(Input.GetKey(KeyCode.Alpha3)) SwitchMode(AnimatorPlayMode.PlayOneShot);
+		if(Input.GetKey(KeyCode.Alpha4)) SwitchMode(AnimatorPlayMode.PingPong);
+		if(Input.GetKey(KeyCode.Alpha5)) SwitchMode(AnimatorPlayMode.MultiRow);
+
+		RenderFrame();
+	}
+
+	void ChangeMode()
+	{
+		SwitchMode(mode);
+	}
+
+	void SwitchMode()
+	{
+		SwitchMode((AnimatorPlayMode)(((int)mode + 1) % (int)AnimatorPlayMode.Total));
+	}
+
+	void SwitchMode(AnimatorPlayMode newMode)
+	{
+		mode = newMode;
+
+		timer = 0.0f;
+
+		switch(mode)
+		{
+			case AnimatorPlayMode.ForwardLoop:
+				reversed = false;
+				loopMode = LoopMode.Loop;
+				useMultiRow = false;
+				break;
+			case AnimatorPlayMode.ReverseLoop:
+				reversed = true;
+				loopMode = LoopMode.Loop;
+				useMultiRow = false;
+				break;
+			case AnimatorPlayMode.PlayOneShot:
+				reversed = false;
+				loopMode = LoopMode.OneShot;
+				useMultiRow = false;
+				currentCol = 0;
+				break;
+			case AnimatorPlayMode.PingPong:
+				reversed = false;
+				loopMode = LoopMode.PingPong;
+				useMultiRow = false;
+				break;
+			case AnimatorPlayMode.MultiRow:
+				reversed = false;
+				loopMode = LoopMode.Loop;
+				useMultiRow = true;
+				break;
+		}
+	}
+
+	void RenderFrame()
+	{
 		Vector2 offset = new Vector2 (1.0f / colSize * (colSize - currentCol - 1), 1.0f / rowSize * (rowSize - currentRow - 1));
 
 		if (timer < 1.0f / framesPerSecond)
@@ -47,9 +132,80 @@ public class CustomAnimator : MonoBehaviour
 		else
 		{
 			timer = 0.0f;
-			currentCol++;
-			currentCol %= (colSize - 1); // Iterate back to first column
+
+			if(reversed) currentCol--;
+			else currentCol++;
+
+			bool goNextRow = true;
+
+			if(loopMode == LoopMode.Loop)
+			{
+				if(currentCol < 0)
+				{
+					currentCol = (colSize - 1); // Iterate back to last column
+				}
+				else if(currentCol >= colSize)
+				{
+					currentCol = 0; // Iterate back to first column
+				}
+				else
+				{
+					goNextRow = false;
+				}
+			}
+			else
+			{
+				if(currentCol <= 0)
+				{
+					if(loopMode == LoopMode.PingPong && !useMultiRow) reversed = !reversed;
+					currentCol = 0; // Stay at the first column
+				}
+				else if(currentCol >= (colSize - 1))
+				{
+					if(loopMode == LoopMode.PingPong && !useMultiRow) reversed = !reversed;
+					currentCol = (colSize - 1); // Stay at the last column
+				}
+				else
+				{
+					goNextRow = false;
+				}
+			}
+
+			if(useMultiRow)
+			{
+				if(goNextRow)
+				{
+					if(reversed) currentRow--;
+					else currentRow++;
+				}
+
+				if(loopMode == LoopMode.Loop)
+				{
+					if(currentRow < 0)
+					{
+						currentRow = (rowSize - 1); // Iterate back to last row
+					}
+					else if(currentRow >= rowSize)
+					{
+						currentRow = 0; // Iterate back to first row
+					}
+				}
+				else
+				{
+					if(currentRow <= 0)
+					{
+						if(loopMode == LoopMode.PingPong) reversed = !reversed;
+						currentRow = 0; // Stay at the first row
+					}
+					else if(currentRow >= (rowSize - 1))
+					{
+						if(loopMode == LoopMode.PingPong) reversed = !reversed;
+						currentRow = (rowSize - 1); // Stay at the last row
+					}
+				}
+			}
 		}
+
 		GetComponent<Renderer> ().material.SetTextureOffset ("_MainTex", offset);
 	}
 }
